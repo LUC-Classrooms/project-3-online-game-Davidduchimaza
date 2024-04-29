e2ee71fa4e380ea7c35eb7405c0b90162e5499bc
Davidduchimaza <151409646+Davidduchimaza@users.noreply.github.com>
2024-04-29 15:58:16 -0500

    completed Box.js

--- Box.js
+++ Box.js
@@ -1,48 +1,38 @@
-function Box(_x, _y){
-    this.x = _x;
-    this.y = _y;
-  
-    this.xSpeed = 0;
-    this.ySpeed = random(1, 2); // 1 - 2 (falling)
-    this.rSpeed = random(-.02, .02); // rotation speed
-  
-    this.angle = 0;
-  
-    /* choose a color scheme at random */
-    if(random(100) > 50){ // 50-50 chance
-      this.boxColor = color(random(100, 255), 0, 0); // red
-      this.ribbonColor = color(0, random(100, 255), 0); // green
-    } else {
-      this.boxColor = color(0, random(100, 255), 0); // green
-      this.ribbonColor = color(random(100, 255), 0, 0); // red
-    }
-  
-    this.display = function(){
-  
-      push();
-      translate(this.x, this.y);
-      rotate(this.angle);
-  
-      rectMode(CENTER);
-      fill(this.boxColor);
-      rect(0, 0, 40); // 40px square
-  
-      fill(this.ribbonColor);
-      rect(0, 0, 40, 10); // horizontal ribbon
-      rect(0, 0, 10, 40); // vertical ribbon
-  
-      pop();
-  
-    }
-  
-    this.move = function() {
-      this.y += this.ySpeed; // spin
-    }
-  
-    this.spin = function() {
-      this.angle += this.rSpeed; // spin
-    }
-  
-  
-  
-  }
+function Box(_x, _y) {
+  this.x = _x;
+  this.y = _y;
+
+  this.xSpeed = 0;
+  this.ySpeed = random(1, 2); // 1 - 2 (falling)
+  this.rSpeed = random(-0.02, 0.02); // rotation speed
+  this.angle = 0;
+
+  this.display = function () {
+    push();
+    translate(this.x, this.y);
+    scale(0.7);
+
+    fill(166, 118, 61); // Cookie color
+    stroke(83, 55, 15); // Chocolate chip color
+    strokeWeight(2);
+
+    // Cookie shape
+    ellipse(0, 0, 80, 60); // Main cookie
+
+    // Chocolate chips
+    ellipse(-20, 0, 10, 10); // Chocolate chip 1
+    ellipse(20, 0, 10, 10); // Chocolate chip 2
+    ellipse(0, -20, 10, 10); // Chocolate chip 3
+    ellipse(0, 20, 10, 10); // Chocolate chip 4
+
+    pop();
+  };
+
+  this.move = function () {
+    this.y += this.ySpeed; // Move down (falling)
+  };
+
+  this.spin = function () {
+    this.angle += this.rSpeed; // Spin
+  };
+}
